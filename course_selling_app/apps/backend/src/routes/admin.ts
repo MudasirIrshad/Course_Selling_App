afbcafef714a61949128d20fba69018e19ddcd46
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-11-26 23:34:19 +0500

    setting backend and packages

--- course_selling_app/apps/backend/src/routes/admin.ts
+++ course_selling_app/apps/backend/src/routes/admin.ts
@@ -5,9 +5,10 @@ import { SECRET_KEY } from "../middleware/index";
 const router = express.Router();
 
 router.get("/get", async (req, res) => {
-  let admin_detail = Admin;
+  let id: number = req.params.admin_id;
+  let admin_detail = await Admin.findById(id);
   if (!admin_detail) res.send({ message: "admin_detail not found" });
-  else res.send({ message: "done"})
+  else res.send({ message: "done" });
 });
 console.log(SECRET_KEY);
 
