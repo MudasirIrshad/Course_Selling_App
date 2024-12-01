f7f9feb63137a094c7ebfdcbebd5a6d584b91953
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-12-01 22:39:41 +0500

    working on backend

--- course_selling_app/apps/backend/src/routes/admin.ts
+++ course_selling_app/apps/backend/src/routes/admin.ts
@@ -7,10 +7,11 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const inputs = AdminZodSchema.safeParse(req.body);
+  if (!inputs.success) res.send("issue in input data");
   let adminname = inputs.data?.adminname;
   let gmail = inputs.data?.gmail;
   let password = inputs.data?.password;
-  let findAdmin = await Admin.findOne({gmail});
+  let findAdmin = await Admin.findOne({ gmail });
   if (findAdmin) res.send({ message: "Admin exits" });
   else {
     const newAdmin = new Admin({
@@ -18,10 +19,28 @@ router.post("/signup", async (req, res) => {
       gmail,
       password,
     });
-    newAdmin.save();
-    res.send(newAdmin);
+    await newAdmin.save();
+    let token = jwt.sign({ adminname, gmail }, SECRET_KEY);
+    res.send({ message: "Admin Signup Token", token });
   }
 });
+
+router.post("/login", async (req, res) => {
+  const inputs = AdminZodSchema.safeParse(req.body);
+  console.log(inputs);
+
+  if (!inputs.success) res.send("issue in input data");
+  let adminname = inputs.data?.adminname;
+  let gmail = inputs.data?.gmail;
+  let password = inputs.data?.password;
+  let findAdmin = await Admin.findOne({ gmail, adminname, password });
+  if (findAdmin) {
+    let token = jwt.sign({ adminname, gmail }, SECRET_KEY);
+    res.send({ message: "Admin Login Token", token });
+  }
+  else res.send("Please Signin first. thankyou")
+});
+
 router.get("/", async (req, res) => {
   let find = await Admin.find();
   res.send({
--- course_selling_app/packages/common/src/index.ts
+++ course_selling_app/packages/common/src/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const AdminZodSchema = z
   .object({
     adminname: z.string().max(20).min(3),
-    gmail: z.string().min(15).max(50),
+    gmail: z.string().min(10).max(50),
     password: z.string().min(8).max(20),
   })
   .strict();
